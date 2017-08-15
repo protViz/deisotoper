89488ea34c1668e5eb39d06f07e5ca4946e00047
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-15 14:46:56 +0200

    DESCRIPTION changed and checkin of deisotoper.R and findpattern.java

--- R/deisotoper.R
+++ R/deisotoper.R
@@ -1,18 +1,31 @@
 #R
 
-# author:
-# date:
+# author: Lucas Schmidt
+# date: 2017-08-15
 
-#' Title blas blas 
-#'
-#' @param mZ 
-#' @param intensity 
-#'
-#' @return
-#' @export summary_psm
-#'
-#' @examples
-summary_psm <- function(mZ, intensity){
-  
-  
-}
+library(rJava)
+library(protViz)
+.jinit()
+.jaddClassPath("/srv/lucas1/eclipse-workspace/deisotoper/bin")
+.jclassPath()
+findpattern <- .jnew("findpattern")
+
+# Loading Data
+url386248 <- "http://fgcz-ms.uzh.ch/~cpanse/data/386248.RData"
+con <- url(url386248)
+load(con)
+S <- as.psmSet(F225712)
+
+# Data
+mZ <- S[[1]]$mZ
+pattern <- c(124.3, 149.5, 251.4)
+eps <- 0.5
+
+# Running Java-Programm with data
+.jcall(findpattern, "V", "setmZ", mZ)
+.jcall(findpattern, "V", "setpattern", pattern)
+.jcall(findpattern, "V", "seteps", eps)
+index <- .jcall(findpattern, "[D", "getIndex")
+
+# Print result
+print(index)
---
+++ R/deisotoper2.R
@@ -0,0 +1,18 @@
+#R
+
+# author:
+# date:
+
+#' Title blas blas 
+#'
+#' @param mZ 
+#' @param intensity 
+#'
+#' @return
+#' @export summary_psm
+#'
+#' @examples
+summary_psm <- function(mZ, intensity){
+  
+  
+}
