de9531bfdf838378480dcc92222674efd2a24375
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-15 16:23:53 +0200

    changes in deisotoper.jar and deisotoper.R

--- R/deisotoper.R
+++ R/deisotoper.R
@@ -17,8 +17,8 @@ summary_psm <- function(mZ, intensity){
   
 }
 
-find_mZpattern <- function(mZ, pattern, eps = 0.01){
-  #      .jinit()
+find_mZpattern <- function(mZ = 1:100, pattern = 1:10, eps = 0.01){
+        .jinit()
   #      .jaddClassPath("/srv/lucas1/eclipse-workspace/deisotoper/bin")
   #      .jclassPath()
   #      findpattern <- .jnew("findpattern")
