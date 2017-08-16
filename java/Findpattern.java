8dfa77d5fb546b7f4d612c071113c29dc4269c2e
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-16 14:04:34 +0200

    commit java/Findpatternbinary.java

--- java/Findpattern.java
+++ java/Findpattern.java
@@ -4,35 +4,31 @@ import java.util.List;
 //Author: Lucas Schmidt
 //Date: 2017-08-15
 
-public class findpattern {
+public class Findpattern {
 	private static double[] mZ;
 	private static double[] pattern;
 	private static double eps;
 
 	public static void setmZ(double[] x) {
-		findpattern.mZ = x;
+		Findpattern.mZ = x;
 	}
 
 	public static void setpattern(double[] x) {
-		findpattern.pattern = x;
+		Findpattern.pattern = x;
 	}
 
 	public static void seteps(double x) {
-		findpattern.eps = x;
+		Findpattern.eps = x;
 	}
 
 	public static double[] find_pattern(double[] mZ, double[] pattern, double eps) {
-		// double[] index = null;
 		List<Double> index = new ArrayList<Double>();
 		for (int i = 0; i < mZ.length; i++) {
-			// System.out.println("i " + i + " mZ " + mZ[i]);
 			for (int j = 0; j < pattern.length; j++) {
 				if (pattern[j] - eps <= mZ[i] && mZ[i] <= pattern[j] + eps) {
-					// System.out.println("j " + j + " mZ " + mZ[i] + " pattern " + pattern[j]);
 					index.add((double) i);
 				}
 			}
-
 		}
 
 		double[] idx = index.stream().mapToDouble(Double::doubleValue).toArray();
@@ -45,18 +41,18 @@ public class findpattern {
 	}
 
 	public static double[] getIndex() {
-		return find_pattern(findpattern.mZ, findpattern.pattern, findpattern.eps);
+		return find_pattern(Findpattern.mZ, Findpattern.pattern, Findpattern.eps);
 	}
 
 	// For Testing only
 	public static void setall() {
-		double[] mZmain = { 3.3, 3.4, 4.1, 4.3, 4.34, 5.3, 5.3456, 7.664 };
-		double[] patternmain = { 4.3 };
-		double epsmain = 0.5;
+		double[] mZmain = { 1, 3, 4, 5, 7, 9, 11, 13, 14, 15, 17.5, 18, 20, 22 };
+		double[] patternmain = {0.6, 1, 5, 6, 23 };
+		double epsmain = 1;
 
-		findpattern.mZ = mZmain;
-		findpattern.pattern = patternmain;
-		findpattern.eps = epsmain;
+		Findpattern.mZ = mZmain;
+		Findpattern.pattern = patternmain;
+		Findpattern.eps = epsmain;
 
 	}
 
