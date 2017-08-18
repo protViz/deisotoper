c03690b9259a827624ac5b56b551c6855cfb3603
Christian Panse <cp@fgcz.ethz.ch>
2017-08-18 13:09:51 +0200

    added findNN docu

--- R/deisotoper.R
+++ R/deisotoper.R
@@ -30,40 +30,58 @@ summary_psm <- function(mZ, intensity){
 #'
 #' @examples
 find_mZpattern <- function(mZ = c(5.5,6.5,7.7,8.8,9.9,10.29), pattern = c(6,8.3), eps = 1){
-  library(rJava)
   .jinit()
   .jaddClassPath("inst/java/deisotoper.jar")
   .jclassPath()
   FindPatternLinear <- .jnew("FindPatternLinear")
   
    # Running Java-Programm with data
-  .jcall("FindPatternLinear", "V", "setMz", mZ)
-  .jcall("FindPatternLinear", "V", "setPattern", pattern)
+  .jcall("FindPatternLinear", "V", "setMz", as.double(mZ))
+  
+  .jcall("FindPatternLinear", "V", "setPattern", as.double(pattern))
+  
   .jcall("FindPatternLinear", "V", "setEps", eps)
-  index <- .jcall("FindPatternLinear", "[D", "getIndex")
+  
+  idx <- .jcall("FindPatternLinear", "[D", "getIndex")
 
-  print(index)
+  idx
 }
 
 
 #' find index of nearest neighbor 
 #'
-#' @param q 
-#' @param vec 
-#' @param check 
+#' @param q a double vector which can be considered as query objects.
+#' @param vec a sorted double vector which can be considered as a data base.
+#' @param check boolean enables test if \code{vec} is sorted. default is \code{FALSE}
 #'
-#' @description 
+#' @description Given a vector of sorted double values \code{vec} of size
+#' \code{n} and a vector of \code{m} query objects \code{q}.
 #'
-#' @seealso 
-#' @return
+#' \code{findNN} determines for each element \code{q[i]} in \code{q}
+#' the nearest neighbor index \code{o} so that the following remains true:
+#'  
+#'  there is no element \code{k} with \code{1} \eqn{\le} \code{k}
+#' \eqn{\le} \code{n} and \code{k} is not \code{o} so that
+#'
+#' \code{abs(vec[k] - q[i])} < \code{abs(vec[o] - q[i])}.
+#' 
+#'  The internal algorithm of \code{findNN} is implemented as binary search.
+#' \code{findNN} has \eqn{O(m * log(n))} time complexity.
+#'
+#'
+#' @seealso \code{link{protViz::findNN}}
+#' @return a integer vector 
 #' @export findNN
 #' 
 #' @examples
+#' 
+#' \dont{
 #' (NNidx <- findNN(q<-c(1, 1.0001, 1.24, 1.26), DB<-seq(1,5,by=0.25)))
 #' (NNidx == c(1,1,2,2))
 #' 
 #' # should be 0
 #' unique(DB[findNN(DB,DB)] - DB)
+#' }
 #' 
 findNN <- function (q, vec, check = FALSE) {
   message("TO BE IMPLEMENTED.")
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,15 @@
 #R
 
+#' Title
+#'
+#' @param lib 
+#' @param pkg 
+#'
+#' @return
+#' @export
+#' @importMethodsFrom packageVersion utils
+#'
+#' @examples
 .onAttach <- function(lib, pkg){
 	if(interactive()){
 		version <- packageVersion('deisotoper')
