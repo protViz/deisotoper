3b64d2e1309f75f40902cf3570727d95cc8d4f06
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-25 13:57:25 +0200

    XIC added to summary

--- java/Summary.java
+++ java/Summary.java
@@ -17,20 +17,26 @@ public class Summary {
 
         for (MassSpectrometryMeasurement.MassSpectrum spectrum : list) {
             List<Double> dist = new ArrayList<>();
+            double intensitysum = 0;
             for (int i = 1; i < spectrum.getMz().length; i++) {
                 dist.add(spectrum.getMz()[i] - spectrum.getMz()[i - 1]);
+            }
+
+            for (double i : spectrum.getIntensity()) {
+                intensitysum += i;
             }
 
             summary.append(spectrumID).append(",nr_of_peaks,").append(spectrum.getMz().length).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",min_intensity,").append(Arrays.stream(spectrum.getIntensity()).min().getAsDouble()).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",max_intensity,").append(Arrays.stream(spectrum.getIntensity()).max().getAsDouble()).append(System.getProperty("line.separator"));
+            summary.append(spectrumID).append(",sum_intensity,").append(intensitysum).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",min_mass,").append(Arrays.stream(spectrum.getMz()).min().getAsDouble()).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",max_mass,").append(Arrays.stream(spectrum.getMz()).max().getAsDouble()).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",min_peak_distance,").append(Collections.min(dist)).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",max_peak_distance,").append(Collections.max(dist)).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",precursor_charge,").append(spectrum.getChargeState()).append(System.getProperty("line.separator"));
             summary.append(spectrumID).append(",precursor_mass,").append(spectrum.getPeptidMass()).append(System.getProperty("line.separator"));
-
+            summary.append(spectrumID).append(",rt_in_seconds,").append(spectrum.getRt()).append(System.getProperty("line.separator"));
             spectrumID++;
         }
 
