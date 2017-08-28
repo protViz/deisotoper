ad5c4159a8953cef486c34a36f8f3c56ca612111
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-28 12:12:09 +0200

    Json To Summary Java App

---
+++ java/JsonToSummary.java
@@ -0,0 +1,10 @@
+/**
+ * @author Lucas Schmidt
+ * @since 2017-08-28
+ */
+
+public class JsonToSummary {
+    public static void main(String[] args) {
+        System.out.println((Summary.makeSummary(GsonMSM.deserializeJsonToMSM(args[0]))));
+    }
+}
