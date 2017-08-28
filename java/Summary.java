66315d91b911860a1c6fdad81d8ca51869ab0684
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-28 12:01:31 +0200

    dry

--- java/Summary.java
+++ java/Summary.java
@@ -18,6 +18,7 @@ public class Summary {
         for (MassSpectrometryMeasurement.MassSpectrum spectrum : list) {
             List<Double> dist = new ArrayList<>();
             double intensity = 0;
+            String linespace = System.getProperty("line.separator");
             for (int i = 1; i < spectrum.getMz().length; i++) {
                 dist.add(spectrum.getMz()[i] - spectrum.getMz()[i - 1]);
             }
@@ -26,17 +27,17 @@ public class Summary {
                 intensity += i;
             }
 
-            summary.append(spectrumID).append(",nr_of_peaks,").append(spectrum.getMz().length).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",min_intensity,").append(Arrays.stream(spectrum.getIntensity()).min().getAsDouble()).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",max_intensity,").append(Arrays.stream(spectrum.getIntensity()).max().getAsDouble()).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",sum_intensity,").append(intensity).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",min_mass,").append(Arrays.stream(spectrum.getMz()).min().getAsDouble()).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",max_mass,").append(Arrays.stream(spectrum.getMz()).max().getAsDouble()).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",min_peak_distance,").append(Collections.min(dist)).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",max_peak_distance,").append(Collections.max(dist)).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",precursor_charge,").append(spectrum.getChargeState()).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",precursor_mass,").append(spectrum.getPeptidMass()).append(System.getProperty("line.separator"));
-            summary.append(spectrumID).append(",rt_in_seconds,").append(spectrum.getRt()).append(System.getProperty("line.separator"));
+            summary.append(spectrumID).append(",nr_of_peaks,").append(spectrum.getMz().length).append(linespace);
+            summary.append(spectrumID).append(",min_intensity,").append(Arrays.stream(spectrum.getIntensity()).min().getAsDouble()).append(linespace);
+            summary.append(spectrumID).append(",max_intensity,").append(Arrays.stream(spectrum.getIntensity()).max().getAsDouble()).append(linespace);
+            summary.append(spectrumID).append(",sum_intensity,").append(intensity).append(linespace);
+            summary.append(spectrumID).append(",min_mass,").append(Arrays.stream(spectrum.getMz()).min().getAsDouble()).append(linespace);
+            summary.append(spectrumID).append(",max_mass,").append(Arrays.stream(spectrum.getMz()).max().getAsDouble()).append(linespace);
+            summary.append(spectrumID).append(",min_peak_distance,").append(Collections.min(dist)).append(linespace);
+            summary.append(spectrumID).append(",max_peak_distance,").append(Collections.max(dist)).append(linespace);
+            summary.append(spectrumID).append(",precursor_charge,").append(spectrum.getChargeState()).append(linespace);
+            summary.append(spectrumID).append(",precursor_mass,").append(spectrum.getPeptidMass()).append(linespace);
+            summary.append(spectrumID).append(",rt_in_seconds,").append(spectrum.getRt()).append(linespace);
             spectrumID++;
         }
 
