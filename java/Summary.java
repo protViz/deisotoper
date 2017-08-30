7823dfe89f2f42abeab3e9059a058a83509e795c
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-08-30 13:14:40 +0200

    DoubleStream

--- java/Summary.java
+++ java/Summary.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.DoubleStream;
 
 public class Summary {
     /**
@@ -17,7 +18,7 @@ public class Summary {
      * @return CSV-formatted string
      * @see MassSpectrometryMeasurement
      */
-    // TODO LS: use spectrum id from MassSpectrum
+    // TODO (LS): use spectrum id from MassSpectrum
     public static String makeSummary(List<MassSpectrometryMeasurement.MassSpectrum> list) {
 
         StringBuilder summary = new StringBuilder();
@@ -27,20 +28,15 @@ public class Summary {
 
         for (MassSpectrometryMeasurement.MassSpectrum spectrum : list) {
             List<Double> dist = new ArrayList<>();
-            double intensity = 0;
+
             for (int i = 1; i < spectrum.getMz().length; i++) {
                 dist.add(spectrum.getMz()[i] - spectrum.getMz()[i - 1]);
-            }
-
-            // TODO (LS): int sum = IntStream.of(a).sum();
-            for (double i : spectrum.getIntensity()) {
-                intensity += i;
             }
 
             summary.append(spectrumID).append(",nr_of_peaks,").append(spectrum.getMz().length).append(linesep);
             summary.append(spectrumID).append(",min_intensity,").append(Arrays.stream(spectrum.getIntensity()).min().getAsDouble()).append(linesep);
             summary.append(spectrumID).append(",max_intensity,").append(Arrays.stream(spectrum.getIntensity()).max().getAsDouble()).append(linesep);
-            summary.append(spectrumID).append(",sum_intensity,").append(intensity).append(linesep);
+            summary.append(spectrumID).append(",sum_intensity,").append(DoubleStream.of(spectrum.getIntensity()).sum()).append(linesep);
             summary.append(spectrumID).append(",min_mass,").append(Arrays.stream(spectrum.getMz()).min().getAsDouble()).append(linesep);
             summary.append(spectrumID).append(",max_mass,").append(Arrays.stream(spectrum.getMz()).max().getAsDouble()).append(linesep);
             summary.append(spectrumID).append(",min_peak_distance,").append(Collections.min(dist)).append(linesep);
