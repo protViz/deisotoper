5f953464a6e70643200d934a6c7b5b84d345b155
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-09-05 08:06:12 +0200

    isotopic prototypes

---
+++ java/src/main/java/ch/fgcz/proteomics/deisotoper/IsotopicClusters.java
@@ -0,0 +1,64 @@
+
+/**
+ * @author Lucas Schmidt
+ * @since 2017-09-05
+ * THIS CODE IS UNDER CONSTRUCTION
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsotopicClusters {
+    private List<List<Double>> isotopicclusters = new ArrayList<>();
+
+    public List<List<Double>> getIsotopicclusters() {
+        return isotopicclusters;
+    }
+
+    public void setIsotopicclusters(List<List<Double>> isotopicclusters) {
+        this.isotopicclusters = isotopicclusters;
+    }
+
+    // TODO: Make it work with IsotopicSets class and change this function to a constructor
+    public static List<List<Double>> createIsotopicClusters(List<Double> isotopicset, double errortolerance) {
+        List<List<Double>> isotopicclusters = new ArrayList<>();
+
+        for (int i = 0; i < isotopicset.size(); i++) {
+            List<Double> isotopiccluster = new ArrayList<>();
+            List<Double> isotopiccluster2 = new ArrayList<>();
+
+            double distance = 0;
+            double distance2 = 0;
+
+            if (i + 1 < isotopicset.size()) {
+                distance = isotopicset.get(i + 1) - isotopicset.get(i);
+            }
+
+            if (i + 2 < isotopicset.size()) {
+                distance2 = isotopicset.get(i + 2) - isotopicset.get(i + 1);
+            }
+
+            for (int charge = 1; charge <= 3; charge++) {
+                if ((1.003 / charge) - errortolerance < distance && distance < (1.003 / charge) + errortolerance) {
+                    isotopiccluster.add(isotopicset.get(i));
+                    isotopiccluster.add(isotopicset.get(i + 1));
+                    if ((1.003 / charge) - errortolerance < distance2 && distance2 < (1.003 / charge) + errortolerance) {
+                        isotopiccluster.add(isotopicset.get(i + 2));
+                    }
+                    break;
+                }
+            }
+
+            if (2 == isotopiccluster.size()) {
+                isotopicclusters.add(isotopiccluster);
+            } else if (3 == isotopiccluster.size()) {
+                isotopicclusters.add(isotopiccluster);
+                isotopiccluster2.addAll(isotopiccluster);
+                isotopiccluster2.remove(2);
+                isotopicclusters.add(isotopiccluster2);
+            }
+        }
+
+        return isotopicclusters;
+    }
+}
---
+++ java/src/main/java/ch/fgcz/proteomics/deisotoper/IsotopicSets.java
@@ -0,0 +1,66 @@
+
+/**
+ * @author Lucas Schmidt
+ * @since 2017-09-05
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsotopicSets {
+    private List<List<Double>> isotopicsets = new ArrayList<>();
+
+    public List<List<Double>> getIsotopicsets() {
+        return isotopicsets;
+    }
+
+    public void setIsotopicsets(List<List<Double>> isotopicsets) {
+        this.isotopicsets = isotopicsets;
+    }
+
+    private static int indexisotopicset = 1;
+    private static int oldindexisotopicset = 1;
+
+    // TODO: Change createIsotopicSets and createIsotopicSet to one function (constructor) and use class IsotopicClusters
+    public List<List<Double>> createIsotopicSets(MassSpectrum massspectrum, double errortolerance) {
+        for (int n = 1; n < massspectrum.getMz().size(); n++) {
+            if (createIsotopicSet(massspectrum, errortolerance).size() > 1) {
+                this.isotopicsets.add(createIsotopicSet(massspectrum, errortolerance));
+            }
+            if (indexisotopicset == oldindexisotopicset + 1) {
+                break;
+            }
+            indexisotopicset = oldindexisotopicset + 1;
+        }
+
+        return isotopicsets;
+    }
+
+    private List<Double> createIsotopicSet(MassSpectrum massspectrum, double errortolerance) {
+        List<Double> isotopicset = new ArrayList<>();
+
+        int j = 0;
+        for (int i = indexisotopicset; i < massspectrum.getMz().size(); i++) {
+            double distance = massspectrum.getMz().get(i) - massspectrum.getMz().get(i - 1);
+            boolean rightdistance = false;
+            for (int charge = 1; charge <= 3; charge++) {
+                if ((1.003 / charge) - errortolerance < distance && distance < (1.003 / charge) + errortolerance) {
+                    if (j == 0) {
+                        isotopicset.add(massspectrum.getMz().get(i - 1));
+                        j++;
+                    }
+                    isotopicset.add(massspectrum.getMz().get(i));
+                    j++;
+                    rightdistance = true;
+                }
+            }
+
+            if (rightdistance == false) {
+                oldindexisotopicset = i;
+                break;
+            }
+        }
+
+        return isotopicset;
+    }
+}
