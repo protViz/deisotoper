f1c30c252f17b5fc62d2fb4deacfef7dbd3d730a
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-09-05 09:35:52 +0200

    refactor

--- java/src/test/java/ch/fgcz/proteomics/deisotoper/IsotopicTest.java
+++ java/src/test/java/ch/fgcz/proteomics/deisotoper/IsotopicTest.java
@@ -13,8 +13,13 @@ import java.util.List;
 import org.junit.Test;
 
 public class IsotopicTest {
-    @Test
-    public void testIsotopeSet() {
+    Peaklist resultpeaklist1;
+    Peaklist resultpeaklist2;
+    Peaklist peaklist1;
+    Peaklist peaklist2;
+    MassSpectrometryMeasurement MSM;
+
+    public void setUp() {
         String source = "IsotopicSetTestData";
 
         // Example 1
@@ -37,37 +42,41 @@ public class IsotopicTest {
         int chargestate2 = 2;
         int id2 = 2;
 
+        // Example 1 Result
+        List<Double> resultmz1 = Arrays.asList(1.33, 1.66, 2.0, 2.5, 3.5, 4.0, 5.0, 6.0);
+        List<Double> resultintensity1 = Arrays.asList(5.0, 6.0, 6.0, 7.0, 7.0, 7.0, 8.0, 8.0);
+        resultpeaklist1 = new Peaklist(resultmz1, resultintensity1);
+
+        // Example 2 Result
+        List<Double> resultmz2 = Arrays.asList(1.0, 2.0, 3.0, 3.5, 4.0, 4.5, 5.0, 6.0, 6.5, 7.0);
+        List<Double> resultintensity2 = Arrays.asList(8.0, 3.0, 5.0, 6.0, 1.0, 4.0, 2.0, 1.0, 4.0, 1.0);
+        resultpeaklist2 = new Peaklist(resultmz2, resultintensity2);
+
         // Create MSM
-        MassSpectrometryMeasurement MSM = new MassSpectrometryMeasurement(source);
+        MSM = new MassSpectrometryMeasurement(source);
         MSM.addMS(typ1, searchengine1, mz1, intensity1, peptidmass1, rt1, chargestate1, id1);
         MSM.addMS(typ2, searchengine2, mz2, intensity2, peptidmass2, rt2, chargestate2, id2);
 
-        // Create IsotopicSets for this MSM (Expected results)
-        List<Double> resultmz1 = Arrays.asList(1.33, 1.66, 2.0, 2.5, 3.5, 4.0, 5.0, 6.0);
-        List<Double> resultintensity1 = Arrays.asList(5.0, 6.0, 6.0, 7.0, 7.0, 7.0, 8.0, 8.0);
-        Peaklist resultpeaklist1 = new Peaklist(resultmz1, resultintensity1);
+        // Create Peaklist of the two MS of MSM
+        peaklist1 = new Peaklist(MSM.getMSlist().get(0));
+        peaklist2 = new Peaklist(MSM.getMSlist().get(1));
+    }
 
-        List<Double> resultmz2 = Arrays.asList(1.0, 2.0, 3.0, 3.5, 4.0, 4.5, 5.0, 6.0, 6.5, 7.0);
-        List<Double> resultintensity2 = Arrays.asList(8.0, 3.0, 5.0, 6.0, 1.0, 4.0, 2.0, 1.0, 4.0, 1.0);
-        Peaklist resultpeaklist2 = new Peaklist(resultmz2, resultintensity2);
-
-        // Create Peaklist of the two MS
+    @Test
+    public void testIsotopeSet() {
+        setUp();
         double errortolerance = 0.01;
-        Peaklist peaklist1 = new Peaklist(MSM.getMSlist().get(0));
-        Peaklist peaklist2 = new Peaklist(MSM.getMSlist().get(1));
 
         // Calculate IsotopicSet
         IsotopicSets IS1 = new IsotopicSets(peaklist1, errortolerance);
-        assertEquals("Calculated IsotopicSet must be same size as defined Result!", IS1.getIsotopicsets().get(0).size(), resultpeaklist1.getPeaklist().size());
+        assertEquals("Calculated IS1 must be same size as defined result (resultpreaklist1)!", IS1.getIsotopicsets().get(0).size(), resultpeaklist1.getPeaklist().size());
 
         IsotopicSets IS2 = new IsotopicSets(peaklist2, errortolerance);
-        assertEquals("Calculated IsotopicSet must be same size as defined Result!", IS2.getIsotopicsets().get(0).size(), resultpeaklist2.getPeaklist().size());
-
+        assertEquals("Calculated IS2 must be same size as defined result (resultpreaklist2)!", IS2.getIsotopicsets().get(0).size(), resultpeaklist2.getPeaklist().size());
     }
 
     @Test
     public void testSpectrumToIsotopeClusters() {
 
     }
-
 }
