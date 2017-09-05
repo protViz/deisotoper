97a2833e41f5bf7ad0109de4b13346d236268654
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-09-05 08:22:45 +0200

    adding isotopictest structure

---
+++ java/src/test/java/ch/fgcz/proteomics/deisotoper/IsotopicTest.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Lucas Schmidt
+ * @since 2017-09-05
+ */
+
+import org.junit.Test;
+
+public class IsotopicTest {
+    @Test
+    public void testIsotopeSet() {
+
+    }
+
+    @Test
+    public void testSpectrumToIsotopeClusters() {
+
+    }
+
+}
