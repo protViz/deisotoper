6f58acae161e33b8d49b9f7f7120dfaa9a11254b
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-09-06 08:39:23 +0200

    javadoc and test

--- java/src/main/java/ch/fgcz/proteomics/deisotoper/IsotopicClusters.java
+++ java/src/main/java/ch/fgcz/proteomics/deisotoper/IsotopicClusters.java
@@ -8,6 +8,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Why is this Class called IsotopicClusters instead of IsotopicSet?
+ * 
+ * The name IsotopicClusters has more reasons. The first reason is, that when you create a IsotopicClusters Object you pass a IsotopicSet (List<Peak>) as parameter to the constructor. That means you
+ * give it a IsotopicSet to make the IsotopicClusters which are in this set. If the Class would be named IsotopicSet, you would give it a IsotopicSet to make a IsotopicSet and that doesn't make any
+ * sense. The next reason is that when you have a Peaklist (List<Peak>) and you create some IsotopicSets from it, the IsotopicSets Object contains a List of IsotopicSet (List<Peak>) and then you pass
+ * one of these IsotopicSets to the constructor to construct the IsotopicClusters for exspecially this IsotopicSet. The Class IsotopicSets and IsotopicClusters are connected by its building structure,
+ * that means that when you want to build clusters from a Peaklist, then you must create firstly a IsotopicSets Object and for each IsotopicSet in this Object you would create a IsotopicClusters
+ * Object and every IsotopicClusters Object has more IsotopicCluster in it. Therefore you could also rename the IsotopicClusters to IsotopicSet, because in this definition it would make sense, but
+ * otherwise it doesn't. The final question is therefore, if it is better to change everything just because of a name and have problems at creation of the Objects, because with other names it would
+ * look much more complicated or change the names to have more logic behind it, when you look at the created Objects? Fact is that the name IsotpicSet and IsotopicClusters are definitely the same, but
+ * only when the clusters are only from one set, which would be correct in this implementation, because you create only isotopicClusters for one passed IsotopicSet. The advantage of this
+ * implementation is simply, that when you have a predefined and not calculated IsotopicSet, you also can pass it to this function and it would make more sense because you get the IsotopicClusters for
+ * exact this IsotopicSet.
+ */
 public class IsotopicClusters {
     private List<List<Peak>> isotopicclusters = new ArrayList<>();
 
@@ -44,11 +59,11 @@ public class IsotopicClusters {
                 if ((1.003 / charge) - errortolerance < distance1 && distance1 < (1.003 / charge) + errortolerance) {
                     isotopiccluster1.add(isotopicset.get(i));
                     isotopiccluster1.add(isotopicset.get(i + 1));
-                    
+
                     if ((1.003 / charge) - errortolerance < distance2 && distance2 < (1.003 / charge) + errortolerance) {
                         isotopiccluster1.add(isotopicset.get(i + 2));
                     }
-                    
+
                     break;
                 }
             }
--- java/src/test/java/ch/fgcz/proteomics/deisotoper/IsotopicTest.java
+++ java/src/test/java/ch/fgcz/proteomics/deisotoper/IsotopicTest.java
@@ -6,6 +6,7 @@
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +20,8 @@ public class IsotopicTest {
     Peaklist peaklist2;
     IsotopicSets IS1;
     IsotopicSets IS2;
+    List<List<Peak>> clusters1 = new ArrayList<>();
+    List<List<Peak>> clusters2 = new ArrayList<>();
 
     /**
      * Sets up the starting parameters.
@@ -49,6 +52,102 @@ public class IsotopicTest {
         List<Double> resultintensity2 = Arrays.asList(8.0, 3.0, 5.0, 6.0, 1.0, 4.0, 2.0, 1.0, 4.0, 1.0);
         resultpeaklist2 = new Peaklist(resultmz2, resultintensity2);
 
+        // Clusters 1 Result
+        List<Double> clustermz1 = Arrays.asList(1.33, 1.66, 2.0);
+        List<Double> clusterintensity1 = Arrays.asList(5.0, 6.0, 6.0);
+        Peaklist cluster1 = new Peaklist(clustermz1, clusterintensity1);
+        clusters1.add(cluster1.getPeaklist());
+        List<Double> clustermz2 = Arrays.asList(1.33, 1.66);
+        List<Double> clusterintensity2 = Arrays.asList(5.0, 6.0);
+        Peaklist cluster2 = new Peaklist(clustermz2, clusterintensity2);
+        clusters1.add(cluster2.getPeaklist());
+        List<Double> clustermz3 = Arrays.asList(1.66, 2.0);
+        List<Double> clusterintensity3 = Arrays.asList(6.0, 6.0);
+        Peaklist cluster3 = new Peaklist(clustermz3, clusterintensity3);
+        clusters1.add(cluster3.getPeaklist());
+        List<Double> clustermz4 = Arrays.asList(2.0, 2.5);
+        List<Double> clusterintensity4 = Arrays.asList(6.0, 7.0);
+        Peaklist cluster4 = new Peaklist(clustermz4, clusterintensity4);
+        clusters1.add(cluster4.getPeaklist());
+        List<Double> clustermz5 = Arrays.asList(2.5, 3.5);
+        List<Double> clusterintensity5 = Arrays.asList(7.0, 7.0);
+        Peaklist cluster5 = new Peaklist(clustermz5, clusterintensity5);
+        clusters1.add(cluster5.getPeaklist());
+        List<Double> clustermz6 = Arrays.asList(3.5, 4.0);
+        List<Double> clusterintensity6 = Arrays.asList(7.0, 7.0);
+        Peaklist cluster6 = new Peaklist(clustermz6, clusterintensity6);
+        clusters1.add(cluster6.getPeaklist());
+        List<Double> clustermz7 = Arrays.asList(4.0, 5.0, 6.0);
+        List<Double> clusterintensity7 = Arrays.asList(7.0, 8.0, 8.0);
+        Peaklist cluster7 = new Peaklist(clustermz7, clusterintensity7);
+        clusters1.add(cluster7.getPeaklist());
+        List<Double> clustermz8 = Arrays.asList(4.0, 5.0);
+        List<Double> clusterintensity8 = Arrays.asList(7.0, 8.0);
+        Peaklist cluster8 = new Peaklist(clustermz8, clusterintensity8);
+        clusters1.add(cluster8.getPeaklist());
+        List<Double> clustermz9 = Arrays.asList(5.0, 6.0);
+        List<Double> clusterintensity9 = Arrays.asList(8.0, 8.0);
+        Peaklist cluster9 = new Peaklist(clustermz9, clusterintensity9);
+        clusters1.add(cluster9.getPeaklist());
+
+        // Clusters 2 Result
+        List<Double> clustermz21 = Arrays.asList(1.0, 2.0, 3.0);
+        List<Double> clusterintensity21 = Arrays.asList(8.0, 3.0, 5.0);
+        Peaklist cluster21 = new Peaklist(clustermz21, clusterintensity21);
+        clusters2.add(cluster21.getPeaklist());
+        List<Double> clustermz22 = Arrays.asList(1.0, 2.0);
+        List<Double> clusterintensity22 = Arrays.asList(8.0, 3.0);
+        Peaklist cluster22 = new Peaklist(clustermz22, clusterintensity22);
+        clusters2.add(cluster22.getPeaklist());
+        List<Double> clustermz23 = Arrays.asList(2.0, 3.0);
+        List<Double> clusterintensity23 = Arrays.asList(3.0, 5.0);
+        Peaklist cluster23 = new Peaklist(clustermz23, clusterintensity23);
+        clusters2.add(cluster23.getPeaklist());
+        List<Double> clustermz24 = Arrays.asList(3.0, 3.5, 4.0);
+        List<Double> clusterintensity24 = Arrays.asList(5.0, 6.0, 1.0);
+        Peaklist cluster24 = new Peaklist(clustermz24, clusterintensity24);
+        clusters2.add(cluster24.getPeaklist());
+        List<Double> clustermz25 = Arrays.asList(3.0, 3.5);
+        List<Double> clusterintensity25 = Arrays.asList(5.0, 6.0);
+        Peaklist cluster25 = new Peaklist(clustermz25, clusterintensity25);
+        clusters2.add(cluster25.getPeaklist());
+        List<Double> clustermz26 = Arrays.asList(3.5, 4.0, 4.5);
+        List<Double> clusterintensity26 = Arrays.asList(6.0, 1.0, 4.0);
+        Peaklist cluster26 = new Peaklist(clustermz26, clusterintensity26);
+        clusters2.add(cluster26.getPeaklist());
+        List<Double> clustermz27 = Arrays.asList(3.5, 4.0);
+        List<Double> clusterintensity27 = Arrays.asList(6.0, 1.0);
+        Peaklist cluster27 = new Peaklist(clustermz27, clusterintensity27);
+        clusters2.add(cluster27.getPeaklist());
+        List<Double> clustermz28 = Arrays.asList(4.0, 4.5, 5.0);
+        List<Double> clusterintensity28 = Arrays.asList(1.0, 4.0, 2.0);
+        Peaklist cluster28 = new Peaklist(clustermz28, clusterintensity28);
+        clusters2.add(cluster28.getPeaklist());
+        List<Double> clustermz29 = Arrays.asList(4.0, 4.5);
+        List<Double> clusterintensity29 = Arrays.asList(1.0, 4.0);
+        Peaklist cluster29 = new Peaklist(clustermz29, clusterintensity29);
+        clusters2.add(cluster29.getPeaklist());
+        List<Double> clustermz210 = Arrays.asList(4.5, 5.0);
+        List<Double> clusterintensity210 = Arrays.asList(4.0, 2.0);
+        Peaklist cluster210 = new Peaklist(clustermz210, clusterintensity210);
+        clusters2.add(cluster210.getPeaklist());
+        List<Double> clustermz211 = Arrays.asList(5.0, 6.0);
+        List<Double> clusterintensity211 = Arrays.asList(2.0, 1.0);
+        Peaklist cluster211 = new Peaklist(clustermz211, clusterintensity211);
+        clusters2.add(cluster211.getPeaklist());
+        List<Double> clustermz212 = Arrays.asList(6.0, 6.5, 7.0);
+        List<Double> clusterintensity212 = Arrays.asList(1.0, 4.0, 1.0);
+        Peaklist cluster212 = new Peaklist(clustermz212, clusterintensity212);
+        clusters2.add(cluster212.getPeaklist());
+        List<Double> clustermz213 = Arrays.asList(6.0, 6.5);
+        List<Double> clusterintensity213 = Arrays.asList(1.0, 4.0);
+        Peaklist cluster213 = new Peaklist(clustermz213, clusterintensity213);
+        clusters2.add(cluster213.getPeaklist());
+        List<Double> clustermz214 = Arrays.asList(6.5, 7.0);
+        List<Double> clusterintensity214 = Arrays.asList(4.0, 1.0);
+        Peaklist cluster214 = new Peaklist(clustermz214, clusterintensity214);
+        clusters2.add(cluster214.getPeaklist());
+
         // Isotopic Sets
         double errortolerance = 0.01;
         IS1 = new IsotopicSets(peaklist1, errortolerance);
@@ -75,25 +174,31 @@ public class IsotopicTest {
      */
     @Test
     public void testIsotopeClustersConstruction() {
-        // Assert
         for (List<Peak> set : IS1.getIsotopicsets()) {
             IsotopicClusters IC = new IsotopicClusters(set, 0.01);
             assertNotNull(IC);
-            // TODO: assertPeaklistEquals with all clusters
+            int i = 0;
+            for (List<Peak> c : IC.getIsotopicclusters()) {
+                assertPeaklistEquals(c, clusters1.get(i)); // Compare each calculated cluster with predefined cluster
+                i++;
+            }
             assertEquals("Calculated IC must be size 9!", IC.getIsotopicclusters().size(), 9);
         }
 
         for (List<Peak> set : IS2.getIsotopicsets()) {
             IsotopicClusters IC = new IsotopicClusters(set, 0.01);
             assertNotNull(IC);
+            int i = 0;
+            for (List<Peak> c : IC.getIsotopicclusters()) {
+                assertPeaklistEquals(c, clusters2.get(i)); // Compare each calculated cluster with predefined cluster
+                i++;
+            }
             assertEquals("Calculated IC must be size 14!", IC.getIsotopicclusters().size(), 14);
         }
     }
 
     // TODO: more unittests for arcs functions (2.2) (DDT)
     // TODO: UML diagram of IS, IC, etc...
-    // TODO: IsotopicClusters to IsotopicSet (overthink whats the best choice)
-    // TODO: Write down Javadoc why IsotopicClusters and not IsotopicSet
     // TODO: Find ways to implement graph structure in java
 
     /**
