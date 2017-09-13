b5c3b78a5bf01999c2a335b6ea0573ec705fe3ca
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-09-13 13:59:43 +0200

    deisotopeMSM

---
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/mspy/Deisotope.java
@@ -0,0 +1,41 @@
+package ch.fgcz.proteomics.mspy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deisotope {
+    public static ch.fgcz.proteomics.dto.MassSpectrometryMeasurement deisotopeMSM(ch.fgcz.proteomics.dto.MassSpectrometryMeasurement input) {
+        // Create a new MSM and deisotope the input and fill it in the new MSM
+        ch.fgcz.proteomics.dto.MassSpectrometryMeasurement output = new ch.fgcz.proteomics.dto.MassSpectrometryMeasurement(input.getSource() + "_output");
+        for (ch.fgcz.proteomics.dto.MassSpectrum ms : input.getMSlist()) {
+            Peaklist inputpeaklist = new Peaklist(ms);
+
+            List<Peak> outputpeaklist = Mspy.deisotope(inputpeaklist.getPeaklist(), 3, 0.05, 0.5, 0.0);
+
+            List<Integer> chargelist = new ArrayList<>();
+            List<Double> isotopelist = new ArrayList<>();
+
+            for (int i = 0; i < outputpeaklist.size(); i++) {
+                chargelist.add(outputpeaklist.get(i).getCharge());
+                isotopelist.add(outputpeaklist.get(i).getIsotope());
+            }
+
+            output.addMS(ms.getTyp(), ms.getSearchEngine(), ms.getMz(), ms.getIntensity(), ms.getPeptidMass(), ms.getRt(), ms.getChargeState(), ms.getId(), chargelist, isotopelist);
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        ch.fgcz.proteomics.dto.MassSpectrometryMeasurement TP_HeLa_200ng_filtered_pd21_MSM = ch.fgcz.proteomics.dto.Serialize
+                .deserializeJsonToMSM("/srv/lucas1/Downloads/TP_HeLa_200ng_filtered_pd21_MSM.json");
+
+        // Make Summary of TP_HeLa_200ng_filtered_pd21_MSM
+        System.out.println("Input summary:");
+        System.out.println(ch.fgcz.proteomics.dto.Summary.makeSummary(TP_HeLa_200ng_filtered_pd21_MSM));
+
+        // Make Summary of TP_HeLa_200ng_filtered_pd21_MSM_output
+        System.out.println("Output summary:");
+        System.out.println(ch.fgcz.proteomics.dto.Summary.makeSummary(Deisotope.deisotopeMSM(TP_HeLa_200ng_filtered_pd21_MSM)));
+    }
+}
