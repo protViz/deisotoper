961fd8b379ce4fd2092ee14864ac18ff6dce6c82
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-09-13 15:51:31 +0200

    add

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/utilities/Convert.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/utilities/Convert.java
@@ -45,8 +45,35 @@ public class Convert {
         return new ch.fgcz.proteomics.mspy.Peaklist(mz, intensity, charge, isotope);
     }
 
+    public static ch.fgcz.proteomics.mspy.Peaklist mgfToPeaklist(String file) {
+        List<Double> mz = new ArrayList<>();
+        List<Double> intensity = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                for (int i = 0; i < parts.length; i++) {
+                    if (i == 0) {
+                        mz.add(Double.parseDouble(parts[i]));
+                    } else if (i == 1) {
+                        intensity.add(Double.parseDouble(parts[i]));
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ch.fgcz.proteomics.mspy.Peaklist(mz, intensity);
+    }
+
     public static void main(String[] args) {
-        ch.fgcz.proteomics.mspy.Peaklist plist = msdToPeaklist("/srv/lucas1/Downloads/output1.msd");
+        ch.fgcz.proteomics.mspy.Peaklist plist2 = msdToPeaklist("/srv/lucas1/Downloads/output1.msd");
+
+        ch.fgcz.proteomics.mspy.Peaklist plist = mgfToPeaklist("/srv/lucas1/Downloads/mgffromhelafiltered");
 
         for (ch.fgcz.proteomics.mspy.Peak e : plist.getPeaklist()) {
             System.out.println("MZ: " + e.getMz() + ", INTENSITY:" + e.getIntensity() + ", CHARGE:" + e.getCharge() + ", ISOTOPE:" + e.getIsotope());
