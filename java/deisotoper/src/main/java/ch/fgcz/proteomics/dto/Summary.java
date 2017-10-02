ba724a08d1105eff78692f433cdb9f058f739691
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-02 09:20:26 +0200

    fix

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/dto/Summary.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/dto/Summary.java
@@ -90,13 +90,8 @@ public class Summary {
                 summary.append(spectrum.getId()).append(",average_charge,").append(chargesum / i).append(linesep);
                 summary.append(spectrum.getId()).append(",average_isotope,").append(isotopesum / i).append(linesep);
                 summary.append(spectrum.getId()).append(",sum_isotope,").append(deisotopesum).append(linesep);
-                if (!spectrum.getCharge().isEmpty() && !spectrum.getIsotope().isEmpty()) {
-                    summary.append(spectrum.getId()).append(",max_charge,").append(Collections.max(spectrum.getCharge())).append(linesep);
-                    summary.append(spectrum.getId()).append(",max_isotope,").append(Collections.max(spectrum.getIsotope())).append(linesep);
-                } else {
-                    summary.append(spectrum.getId()).append(",max_charge,").append(0).append(linesep);
-                    summary.append(spectrum.getId()).append(",max_isotope,").append(0).append(linesep);
-                }
+                summary.append(spectrum.getId()).append(",max_charge,").append(Collections.max(spectrum.getCharge())).append(linesep);
+                summary.append(spectrum.getId()).append(",max_isotope,").append(Collections.max(spectrum.getIsotope())).append(linesep);
                 summary.append(spectrum.getId()).append(",nr_z1,").append(z1sum).append(linesep);
                 summary.append(spectrum.getId()).append(",nr_z2,").append(z2sum).append(linesep);
                 summary.append(spectrum.getId()).append(",nr_z3,").append(z3sum).append(linesep);
@@ -105,5 +100,23 @@ public class Summary {
         }
 
         return summary.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "ZZ";
+        String typ = "XX";
+        String searchengine = "YY";
+        double[] mz = {};
+        double[] intensity = {};
+        double peptidmass = 0;
+        double rt = 0;
+        int chargestate = 2;
+        int id = 0;
+
+        MassSpectrometryMeasurement test = new MassSpectrometryMeasurement(s);
+        test.addMS(typ, searchengine, mz, intensity, peptidmass, rt, chargestate, id);
+
+        System.out.println(makeSummary(test));
+        System.out.println(makeSummary(ch.fgcz.proteomics.fdbm.Deisotope.deisotopeMSM(test, false, "first")));
     }
 }
