54967af169b0f69b4d4bba9eff6ea3a89d3232d8
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-06 13:35:26 +0200

    bugfix

--- bash/rBenchmark.R
+++ bash/rBenchmark.R
@@ -6,7 +6,7 @@ library(deisotoper)
 
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm")
 if(file.exists("/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")) {
-  deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm", aamassfile = "/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")
+  deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm_with_mod.mgf", sep = ""), method = "fdbm", aamassfile = "/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")
 }
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_mspy.mgf", sep = ""), method = "mspy")
 
