584d295080cbb6d2159aab8b0d90dac85dc30581
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-06 09:46:04 +0200

    fix

--- bash/rBenchmark.R
+++ bash/rBenchmark.R
@@ -1,6 +1,7 @@
 #!/usr/bin/Rscript
 
 args <- commandArgs(TRUE)
+
 library(deisotoper)
 
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm")
