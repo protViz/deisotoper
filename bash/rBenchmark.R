9dc613b82e7230c516bf20bd18393e5d9c960176
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-06 12:50:51 +0200

    fix

--- bash/rBenchmark.R
+++ bash/rBenchmark.R
@@ -7,6 +7,7 @@ library(deisotoper)
 rm(list=ls())
 
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm")
+deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm", aamassfile = "/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_mspy.mgf", sep = ""), method = "mspy")
 
 rm(list=ls())
