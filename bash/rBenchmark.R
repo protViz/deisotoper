8a608483ba920c1550426afcf285e71c41ed6411
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-06 12:52:55 +0200

    typo

--- bash/rBenchmark.R
+++ bash/rBenchmark.R
@@ -7,8 +7,9 @@ library(deisotoper)
 rm(list=ls())
 
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm")
-if(file.exists("/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini"))
-deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm", aamassfile = "/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")
+if(file.exists("/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")) {
+  deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm.mgf", sep = ""), method = "fdbm", aamassfile = "/srv/lucas1/deisotoper/bash/AminoAcidMasses.ini")
+}
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_mspy.mgf", sep = ""), method = "mspy")
 
 rm(list=ls())
