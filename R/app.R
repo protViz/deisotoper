12c6a3e30dcdc12012e1d967f26235033a0b7b61
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-10 13:16:34 +0200

    shiny

--- R/app.R
+++ R/app.R
@@ -1,23 +1,28 @@
 library(shiny)
 
 ui <- fluidPage(
-  titlePanel("PSM-Difference"),
-  sidebarLayout(
-    sidebarPanel(textInput("text1", "URL1", "http://fgcz-ms.uzh.ch/~cpanse/R257544/TP_HeLa_200ng_msdeconv.RData"),
-                 actionButton("load1", "load"),
-                 br(),
-                 br(),
-                 textInput("text2", "URL2", "http://fgcz-ms.uzh.ch/~cpanse/R257544/TP_HeLa_200ng_msdeconv.RData"),
-                 actionButton("load2", "load"),
-                 br(),
-                 br(),
-                 actionButton("action", "+")),
-    mainPanel(plotOutput("plot1"), plotOutput("plot2"))
+  titlePanel("PSM-Difference",windowTitle = "PSM-Difference"),
+  fluidRow(
+    column(12, align="center",
+           hr(),
+           textInput("text1", "URL1", "file:///srv/lucas1/Downloads/MascotDaemon/deisotoped/fdbmaggregatednew/TP_HeLa_200ng_benchmark_fdbm_first.RData", width = "100%"),
+           actionButton("load1", "load", width = "10%", style="center-align"),
+           hr(),
+           textInput("text2", "URL2", "file:///srv/lucas1/Downloads/MascotDaemon/deisotoped/fdbmaggregatednew/TP_HeLa_200ng_benchmark_fdbm_with_mod_first.RData", width = "100%"),
+           actionButton("load2", "load", width = "10%"),
+           hr(),
+           div(style="display: inline-block;vertical-align:top; width: 150px;", actionButton("action1", "-", width = "30%")),
+           div(style="display: inline-block;vertical-align:top; width: 150px;",textOutput("num")),
+           div(style="display: inline-block;vertical-align:top; width: 150px;",actionButton("action2", "+", width = "30%")),
+           hr(),
+           plotOutput("plot1", height = "520"), 
+           plotOutput("plot2", height = "520")
+    )
   )
 )
 
 server <- function(input, output, session) {
-  values <- reactiveValues(n = 0)
+  values <- reactiveValues(i = 1)
   
   button1 <- eventReactive(input$load1, {
     input$text2
@@ -28,19 +33,28 @@ server <- function(input, output, session) {
   })
   
   observe({
-    input$action
+    input$action2
     isolate({
-      values$n <- values$n + 1
+      values$i <- values$i + 1
     })
   })
   
+  observe({
+    input$action1
+    isolate(if(values$i != 1) { values$i <- values$i - 1 })
+  })
+  
+  output$num <- renderText({
+    values$i
+  })
+  
   output$plot1 <- renderPlot({
-    val <- get(load(con <- url(button1())))[[values$n]]
+    val <- get(load(con <- url(button1())))[[values$i]]
     plot(x = val$mZ, y = val$intensity, type = "h", xlab = "mZ", ylab = "Intensity", main = val$title)
   })
   
   output$plot2 <- renderPlot({   
-    val <- get(load(con <- url(button2())))[[values$n]]
+    val <- get(load(con <- url(button2())))[[values$i]]
     plot(x = val$mZ, y = val$intensity, type = "h", xlab = "mZ", ylab = "Intensity", main = val$title)  
   })
 }
