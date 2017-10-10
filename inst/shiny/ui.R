96de04cfd8a1f68a3cb9de647480f34ab98d575a
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-10 14:45:06 +0200

    change

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-ui <- fluidPage(
+shinyUI(fluidPage(
   titlePanel("PSM-Difference",windowTitle = "PSM-Difference"),
   fluidRow(
     column(12, align="center",
@@ -20,43 +20,4 @@ ui <- fluidPage(
     )
   )
 )
-
-server <- function(input, output, session) {
-  values <- reactiveValues(i = 1)
-  
-  button1 <- eventReactive(input$load1, {
-    input$text2
-  })
-  
-  button2 <- eventReactive(input$load2, {
-    input$text2
-  })
-  
-  observe({
-    input$action2
-    isolate({
-      values$i <- values$i + 1
-    })
-  })
-  
-  observe({
-    input$action1
-    isolate(if(values$i != 1) { values$i <- values$i - 1 })
-  })
-  
-  output$num <- renderText({
-    values$i
-  })
-  
-  output$plot1 <- renderPlot({
-    val <- get(load(con <- url(button1())))[[values$i]]
-    plot(x = val$mZ, y = val$intensity, type = "h", xlab = "mZ", ylab = "Intensity", main = val$title)
-  })
-  
-  output$plot2 <- renderPlot({   
-    val <- get(load(con <- url(button2())))[[values$i]]
-    plot(x = val$mZ, y = val$intensity, type = "h", xlab = "mZ", ylab = "Intensity", main = val$title)  
-  })
-}
-
-shinyApp(ui = ui, server = server)
+)
