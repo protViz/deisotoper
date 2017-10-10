0ff7555c77c1d9ee2c3807d1207a1bcea40b9718
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-10 15:22:22 +0200

    change

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -5,11 +5,11 @@ shinyServer(function(input, output, session) {
   values <- reactiveValues(i = 1)
   
   button1 <- eventReactive(input$load1, {
-    input$text1
+    get(load(con <- url(input$text1)))
   })
   
   button2 <- eventReactive(input$load2, {
-    input$text2
+    get(load(con <- url(input$text2)))
   })
   
   observe({
@@ -31,17 +31,15 @@ shinyServer(function(input, output, session) {
   output$plot1 <- renderPlot({
     #val <- get(load(con <- url(button1())))[[values$i]]
     #plot(x = val$mZ, y = val$intensity, type = "h", xlab = "mZ", ylab = "Intensity", main = val$title)
-    u <- button1()
-    val <- get(load(con <- url(u)))
-    plot.mascot_query(val$queries[[values$i]], val)
+    val <- button1()
+    rv <- plot.mascot_query(val$queries[[values$i]], val)
   })
   
   output$plot2 <- renderPlot({   
     #val <- get(load(con <- url(button2())))[[values$i]]
     #plot(x = val$mZ, y = val$intensity, type = "h", xlab = "mZ", ylab = "Intensity", main = val$title)
-    u <- button2()
-    val <- get(load(con <- url(u)))
-    plot.mascot_query(val$queries[[values$i]], val)
+    val <- button2()
+    rv <- plot.mascot_query(val$queries[[values$i]], val)
   })
 }
 )
