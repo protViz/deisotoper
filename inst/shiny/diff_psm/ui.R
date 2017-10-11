b02ef1469f5fd1487db71ad745a5f593cbcc1a92
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-11 14:57:54 +0200

    style

--- inst/shiny/diff_psm/ui.R
+++ inst/shiny/diff_psm/ui.R
@@ -12,16 +12,20 @@ shinyUI(fluidPage(
       hr(),
       textInput("text1", "URLs", "http://fgcz-ms.uzh.ch/~cpanse/R257544/F257664.RData", width = "100%"),
       textInput("text2", NULL, value = "http://fgcz-ms.uzh.ch/~cpanse/R257544/F257544.RData", width = "100%"),
-      actionButton("load1", "load", width = "10%"),
-      radioButtons("sort", NULL, choiceNames = c("Sort first", "Sort second"), choiceValues = c(1, 2)),
+      radioButtons("sort", NULL, choiceNames = c("Sort first", "Sort second", "Sort none"), choiceValues = c(1, 2, 3), inline = TRUE),
+      actionButton("load1", "load & sort", width = "150px"),
+      
       hr(),
+      
       div(style="display: inline-block;vertical-align:top; width: 10%;", actionButton("action1", "-", width = "50px")),
-      div(style="display: inline-block;vertical-align:top; width: 10%;", textOutput("num"), width = "50px"),
+      div(style="display: inline-block;vertical-align:top; width: 3%;font-size:2em;", textOutput("num"), width = "50px"),
       div(style="display: inline-block;vertical-align:top; width: 10%;", actionButton("action2", "+", width = "50px")),
-      div(style="display: inline-block;vertical-align:top; width: 65%;", sliderInput("zoom", label = NULL, min = 0, max = 2000, value = c(0, 2000), width = "100%")),
+      div(style="display: inline-block;vertical-align:top; width: 75%;", sliderInput("zoom", label = NULL, min = 0, max = 2000, value = c(0, 2000), width = "100%")),
+     
       hr(),
-      plotOutput("plot1", height = "520"), 
-      plotOutput("plot2", height = "520")
+      
+      plotOutput("plot1", height = "510"), 
+      plotOutput("plot2", height = "510")
       )
     )
   )
