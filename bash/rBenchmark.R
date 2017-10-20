c9bc88ef1b2a2ba08c4a420e2e3878cd2575ddbc
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-10-20 09:44:24 +0200

    properties 1

--- bash/rBenchmark.R
+++ bash/rBenchmark.R
@@ -5,8 +5,8 @@ args <- commandArgs(TRUE)
 library(deisotoper)
 
 deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm_first.mgf", sep = ""), method = "fdbm", modus="first")
-if(file.exists("/srv/lucas1/deisotoper/ini/AminoAcidMassesWithCarbamidomethylDeamidationOxidation.ini")) {
-  deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm_with_mod_first.mgf", sep = ""), method = "fdbm", modus="first", aamassfile = "/srv/lucas1/deisotoper/ini/AminoAcidMassesWithCarbamidomethylDeamidationOxidation.ini")
+if(file.exists("/srv/lucas1/deisotoper/properties/AminoAcidMassesWithCarbamidomethylDeamidationOxidation.properties")) {
+  deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_fdbm_with_mod_first.mgf", sep = ""), method = "fdbm", modus="first", aamassfile = "/srv/lucas1/deisotoper/properties/AminoAcidMassesWithCarbamidomethylDeamidationOxidation.properties")
 }
 #deisotoper:::jBenchmark(input = args[[1]], output = paste(args[[2]], "_mspy.mgf", sep = ""), method = "mspy")
 
