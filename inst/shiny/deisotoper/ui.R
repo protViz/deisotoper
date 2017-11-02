c983dee9f660cc36b40cf0b9abbb1c4f01ba539b
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-02 12:46:44 +0100

    alignment

--- inst/shiny/deisotoper/ui.R
+++ inst/shiny/deisotoper/ui.R
@@ -1,6 +1,7 @@
 shinyUI(fluidPage(
   titlePanel("DEISOTOPER", windowTitle = "DEISOTOPER"),
-  fluidRow( hr(),
+  fluidRow(
+    hr(),
     column(
       12,
       align="middle",
@@ -32,13 +33,14 @@ shinyUI(fluidPage(
                  hr(),
                  tableOutput("outputstatistic")),
         tabPanel("Configuration", 
-                 htmlOutput("explanation", container = ),
+                 htmlOutput("explanation"),
+                 hr(),
                  textAreaInput("config", "Configuration", "", width = "100%", height = "500", resize = "none", placeholder = "For example:
                                ALA=71.03711
                                               F1=0.8
                                               F2=0.5
                                               F3=0.1
-                               DISTANCE=1.003")
+                               DISTANCE=1.003"), hr()
         )
         )
       ))
