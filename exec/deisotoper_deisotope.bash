394eea65987b0750e03ff1ef655eb147b23e37bb
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-03 15:17:52 +0100

    bash

--- exec/deisotoper_deisotope.bash
+++ exec/deisotoper_deisotope.bash
@@ -1,15 +1,9 @@
 #!/bin/bash
+# Usage: cat in.mgf | deisotoper_deisotope.bash > out.mgf
+TEMPFILEIN=$( mktemp )
+TEMPFILEOUT=$( mktemp )
+cat > "$TEMPFILEIN" 
+java -cp '../inst/java/ExecDeisotoper.jar:../inst/java/deisotoper-1.0-SNAPSHOT.jar:../inst/java/jgrapht-core-1.0.1.jar' ExecDeisotoper "$TEMPFILEIN" "$TEMPFILEOUT"
+cat "$TEMPFILEOUT"
+rm -f "$TEMPFILEIN" "$TEMPFILEOUT"
 
-
-# INPUT  mgf
-# OUTPUT mgf
-# TODO
-
-# java my.jar /dev/stdin /dev/stdout
-
-javac -cp '.:../inst/java/deisotoper-1.0-SNAPSHOT.jar' ExecDeisotoper.java
-jar -cf ExecDeisotoper.jar ExecDeisotoper
-
-
-
-java -cp 'ExecDeisotoper.jar:../inst/java/deisotoper-1.0-SNAPSHOT.jar:../inst/java/jgrapht-core-1.0.1.jar' ExecDeisotoper test.mgf test333.mgf
