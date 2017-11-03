d83320872b993f41044971cc13bf9f8013688f99
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-03 15:20:37 +0100

    cosmetics

--- exec/deisotoper_deisotope.bash
+++ exec/deisotoper_deisotope.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Usage: cat in.mgf | deisotoper_deisotope.bash > out.mgf
+# Usage: cat in.mgf | ./deisotoper_deisotope.bash > out.mgf
 TEMPFILEIN=$( mktemp )
-TEMPFILEOUT=$( mktemp )
+TEMPFILEOUT=$( mktemp -u )
 cat > "$TEMPFILEIN" 
 java -cp '../inst/java/ExecDeisotoper.jar:../inst/java/deisotoper-1.0-SNAPSHOT.jar:../inst/java/jgrapht-core-1.0.1.jar' ExecDeisotoper "$TEMPFILEIN" "$TEMPFILEOUT"
 cat "$TEMPFILEOUT"
