f035bf28804502a93f9bc0b774220cf52c9b4ad8
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-03 14:26:05 +0100

    changes

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/R/MascotGenericFormatR.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/R/MascotGenericFormatR.java
@@ -1,14 +1,13 @@
 package ch.fgcz.proteomics.R;
 
-import ch.fgcz.proteomics.dto.MassSpectrometryMeasurement;
-import ch.fgcz.proteomics.fbdm.Deisotoper;
-import ch.fgcz.proteomics.mgf.ReadMGF;
-import ch.fgcz.proteomics.mgf.WriteMGF;
-
 /**
  * @author Lucas Schmidt
  * @since 2017-11-03
  */
+
+import ch.fgcz.proteomics.dto.MassSpectrometryMeasurement;
+import ch.fgcz.proteomics.mgf.ReadMGF;
+import ch.fgcz.proteomics.mgf.WriteMGF;
 
 public class MascotGenericFormatR {
     public static MassSpectrometryMeasurement readR(String file) {
@@ -17,18 +16,5 @@ public class MascotGenericFormatR {
 
     public static boolean writeR(String file, MassSpectrometryMeasurement msm) {
         return WriteMGF.write(file, msm);
-    }
-
-    // USAGE: java MGFR <INPUTFILE> <OUTPUTFILE>
-    public static void main(String[] args) {
-        if (args.length == 2) {
-            MassSpectrometryMeasurement msm = readR(args[0]);
-
-            MassSpectrometryMeasurement msmd = Deisotoper.deisotopeMSM(msm, "first", "nofile");
-
-            writeR(args[1], msmd);
-        } else {
-            System.err.println("WARNING: The arguments are missing (first argument: inputfile, second argument: outputfile)!");
-        }
     }
 }
