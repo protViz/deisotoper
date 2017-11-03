6da07055f00dbbf938679a09f9063b5277396f6a
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-03 14:53:39 +0100

    changes

--- exec/deisotoper_deisotope.bash
+++ exec/deisotoper_deisotope.bash
@@ -6,3 +6,8 @@
 # TODO
 
 # java my.jar /dev/stdin /dev/stdout
+
+javac -cp '.:../inst/java/deisotoper-1.0-SNAPSHOT.jar:../inst/java/*.jar' ExecDeisotoper.java
+jar -cf ExecDeisotoper.jar ExecDeisotoper
+java -cp '.:../inst/java/deisotoper-1.0-SNAPSHOT.jar:../inst/java/*.jar' ExecDeisotoper
+
