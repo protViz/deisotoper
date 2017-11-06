e67920a3d3748967b72532a75e9b45adc7a72353
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-06 08:32:59 +0100

    changes for std

--- exec/ExecDeisotoper.java
+++ exec/ExecDeisotoper.java
@@ -9,17 +9,12 @@ import ch.fgcz.proteomics.mgf.ReadMGF;
 import ch.fgcz.proteomics.mgf.WriteMGF;
 
 public class ExecDeisotoper {
-    // USAGE: java MGFR <INPUTFILE> <OUTPUTFILE>
     public static void main(String[] args) {
-        if (args.length == 2) {
-            MassSpectrometryMeasurement msm = ReadMGF.read(args[0]);
+        MassSpectrometryMeasurement msm = ReadMGF.read();
 
-            MassSpectrometryMeasurement msmd = Deisotoper.deisotopeMSM(msm, "first", "nofile");
+        MassSpectrometryMeasurement msmd = Deisotoper.deisotopeMSM(msm, "first", "nofile");
 
-            WriteMGF.write(args[1], msmd);
-        } else {
-            System.err.println("WARNING: The arguments are missing (first argument: inputfile, second argument: outputfile)!");
-        }
+        WriteMGF.write(msmd);
     }
 }
 
