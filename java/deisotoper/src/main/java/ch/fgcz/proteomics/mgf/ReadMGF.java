9df15cb37718f130299b99ce0d8fcddea7d45d14
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-06 08:31:45 +0100

    changes for stdin

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/mgf/ReadMGF.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/mgf/ReadMGF.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,6 +17,7 @@ import java.util.Scanner;
 import ch.fgcz.proteomics.dto.MassSpectrometryMeasurement;
 
 public class ReadMGF {
+    // FILE
     private static String readHeader(String file) {
         try (BufferedReader bufferedreader = new BufferedReader(new FileReader(file))) {
             String line = bufferedreader.readLine();
@@ -39,6 +41,7 @@ public class ReadMGF {
         return null;
     }
 
+    // FILE
     private static MassSpectrometryMeasurement readLocal(String file, MassSpectrometryMeasurement MSM) {
         try (BufferedReader bufferedreader = new BufferedReader(new FileReader(file))) {
             String line = bufferedreader.readLine();
@@ -91,6 +94,84 @@ public class ReadMGF {
         return MSM;
     }
 
+    // STDIN
+    private static String readHeader() {
+        try (BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = bufferedreader.readLine();
+            String[] partequal = line.split("=");
+
+            while (line != null) {
+                if (line.equals("BEGIN IONS")) {
+                    break;
+                } else if (line.contains("COM")) {
+                    return partequal[1];
+                }
+
+                line = bufferedreader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // STDIN
+    private static MassSpectrometryMeasurement readLocal(MassSpectrometryMeasurement MSM) {
+        try (BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = bufferedreader.readLine();
+            int chargestate = 0;
+            int id = 0;
+            String searchengine = null;
+            String typ = null;
+            double peptidmass = 0;
+            double rt = 0;
+            List<Double> mz = new ArrayList<>();
+            List<Double> intensity = new ArrayList<>();
+
+            while (line != null) {
+                String[] partequal = line.split("=");
+                String[] partspace = line.split(" ");
+
+                if (line.equals("BEGIN IONS")) {
+                    chargestate = 0;
+                    id = MSM.getMSlist().size();
+                    searchengine = null;
+                    typ = null;
+                    peptidmass = 0;
+                    rt = 0;
+                    mz = new ArrayList<>();
+                    intensity = new ArrayList<>();
+                } else if (line.equals("END IONS")) {
+                    MSM.addMS(typ, searchengine, mz, intensity, peptidmass, rt, chargestate, id);
+                } else if (line.contains("CHARGE")) {
+                    chargestate = Integer.parseInt(partequal[1].substring(0, 1));
+                } else if (line.contains("TITLE")) {
+                    typ = partequal[1];
+                } else if (line.contains("RTINSECONDS")) {
+                    rt = Double.parseDouble(partequal[1]);
+                } else if (line.contains("PEPMASS")) {
+                    String[] pepmasssplit = partequal[1].split(" ");
+                    peptidmass = Double.parseDouble(pepmasssplit[0]);
+                } else if (isDouble(partspace[0])) {
+                    mz.add(Double.parseDouble(partspace[0]));
+                    intensity.add(Double.parseDouble(partspace[1]));
+                }
+
+                line = bufferedreader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return MSM;
+    }
+
+    // FILE
     public static MassSpectrometryMeasurement read(String file) {
         String source = readHeader(file);
 
@@ -101,6 +182,21 @@ public class ReadMGF {
         MassSpectrometryMeasurement MSM = new MassSpectrometryMeasurement(source);
 
         MSM = readLocal(file, MSM);
+
+        return MSM;
+    }
+
+    // STDIN
+    public static MassSpectrometryMeasurement read() {
+        String source = readHeader();
+
+        if (source == null) {
+            source = "stdin";
+        }
+
+        MassSpectrometryMeasurement MSM = new MassSpectrometryMeasurement(source);
+
+        MSM = readLocal(MSM);
 
         return MSM;
     }
--- java/deisotoper/src/main/java/ch/fgcz/proteomics/mgf/WriteMGF.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/mgf/WriteMGF.java
@@ -9,6 +9,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,6 +18,7 @@ import ch.fgcz.proteomics.dto.MassSpectrometryMeasurement;
 import ch.fgcz.proteomics.dto.MassSpectrum;
 
 public class WriteMGF {
+    // FILE
     private static boolean writeHeader(File file, MassSpectrometryMeasurement MSM) {
         try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))) {
             out.println("# deisotoped by fbdm algorithm at " + new SimpleDateFormat("yyyy-MM-dd:HH-mm").format(new Date()));
@@ -27,6 +29,7 @@ public class WriteMGF {
         return true;
     }
 
+    // FILE
     private static boolean writeLocal(File file, MassSpectrometryMeasurement MSM) {
         try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))) {
             for (MassSpectrum MS : MSM.getMSlist()) {
@@ -49,6 +52,7 @@ public class WriteMGF {
         return true;
     }
 
+    // FILE
     private static boolean writeConnect(File file, MassSpectrometryMeasurement MSM) {
         boolean header = writeHeader(file, MSM);
         boolean local = writeLocal(file, MSM);
@@ -60,6 +64,48 @@ public class WriteMGF {
         }
     }
 
+    // STDOUT
+    private static boolean writeHeader(MassSpectrometryMeasurement MSM) {
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        out.println("# deisotoped by fbdm algorithm at " + new SimpleDateFormat("yyyy-MM-dd:HH-mm").format(new Date()));
+        out.println("COM=" + MSM.getSource());
+
+        return true;
+    }
+
+    // STDOUT
+    private static boolean writeLocal(MassSpectrometryMeasurement MSM) {
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        for (MassSpectrum MS : MSM.getMSlist()) {
+            out.println("BEGIN IONS");
+
+            out.println("TITLE=" + MS.getTyp());
+            out.println("PEPMASS=" + MS.getPeptideMass());
+            out.println("CHARGE=" + MS.getChargeState() + "+");
+            out.println("RTINSECONDS=" + MS.getRt());
+            int i = 0;
+            for (i = 0; i < MS.getMz().size(); i++) {
+                out.println(MS.getMz().get(i) + " " + MS.getIntensity().get(i));
+            }
+            out.println("END IONS");
+        }
+
+        return true;
+    }
+
+    // STDOUT
+    private static boolean writeConnect(MassSpectrometryMeasurement MSM) {
+        boolean header = writeHeader(MSM);
+        boolean local = writeLocal(MSM);
+
+        if (local == false || header == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // FILE
     public static boolean write(String file, MassSpectrometryMeasurement MSM) {
         File f = new File(file);
 
@@ -69,5 +115,10 @@ public class WriteMGF {
         }
 
         return writeConnect(f, MSM);
+    }
+
+    // STDOUT
+    public static boolean write(MassSpectrometryMeasurement MSM) {
+        return writeConnect(MSM);
     }
 }
