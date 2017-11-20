15fa4185f86577d1ed5e4ecbbabb993493c4de94
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-20 10:18:04 +0100

    adjustment plot

--- R/deisotoper2.R
+++ R/deisotoper2.R
@@ -140,12 +140,12 @@ getConfig <- function(deisotoper) {
   maxintensity2 <- max(massspectrum2$intensity)
   
   if(maxintensity1 <= maxintensity2) {
-    plot(x = massspectrum2$mZ, y = massspectrum2$intensity, type = "h", xlab = "mZ", ylab = "Intensity", col = "#0000FF99", axes = FALSE, xlim = zoom)
+    plot(x = massspectrum2$mZ, y = massspectrum2$intensity, type = "h", xlab = "mZ", ylab = "Intensity", col = "#0000FF99", axes = FALSE, xlim = zoom, ylim = c(0, maxintensity2 + 100))
     lines(x = massspectrum1$mZ, y = massspectrum1$intensity, type = "h", xlab = "mZ", ylab = "Intensity", col = "#FF000099")
     mtext(text ="First Mass Spectrum", line = 2, adj = 0, col="blue")
     mtext(text ="Second Mass Spectrum", line = 1, adj = 0, col="red")
   } else if (maxintensity1 > maxintensity2) {
-    plot(x = massspectrum1$mZ, y = massspectrum1$intensity, type = "h", xlab = "mZ", ylab = "Intensity", col = "#FF000099", axes = FALSE, xlim = zoom)
+    plot(x = massspectrum1$mZ, y = massspectrum1$intensity, type = "h", xlab = "mZ", ylab = "Intensity", col = "#FF000099", axes = FALSE, xlim = zoom, ylim = c(0, maxintensity1 + 100))
     lines(x = massspectrum2$mZ, y = massspectrum2$intensity, type = "h", xlab = "mZ", ylab = "Intensity", col = "#0000FF99")
     mtext(text ="First Mass Spectrum", line = 2, adj = 0, col="red")
     mtext(text ="Second Mass Spectrum", line = 1, adj = 0, col="blue")
