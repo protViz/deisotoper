b50d7f7cb0fb1667fc3734c113a692b89b67b7ca
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-11-23 10:13:52 +0100

    removed old not working functions

--- R/deisotoper.R
+++ R/deisotoper.R
@@ -258,3 +258,45 @@ getConfig <- function(deisotoper) {
   
   version
 }
+
+#' find index of nearest neighbor.
+#'
+#' @param q a double vector which can be considered as query objects.
+#' @param vec a sorted double vector which can be considered as a data base.
+#'
+#' @description Given a vector of sorted double values \code{vec} of size
+#' \code{n} and a vector of \code{m} query objects \code{q}.
+#'
+#' \code{findNN} determines for each element \code{q[i]} in \code{q}
+#' the nearest neighbor index \code{o} so that the following remains true:
+#'  
+#'  there is no element \code{k} with \code{1} \eqn{\le} \code{k}
+#' \eqn{\le} \code{n} and \code{k} is not \code{o} so that
+#'
+#' \code{abs(vec[k] - q[i])} < \code{abs(vec[o] - q[i])}.
+#' 
+#'  The internal algorithm of \code{findNN} is implemented as binary search.
+#' \code{findNN} has \eqn{O(m * log(n))} time complexity.
+#'
+#' @author Lucas Schmidt, Christian Panse
+#' @seealso \code{protViz::findNN}'s cpluplus implementation.
+#' @return an integer vector 
+#' @export findNN
+#' 
+#' @examples
+#' (NNidx <- findNN(q<-c(1, 1.0001, 1.24, 1.26), DB<-seq(1,5,by=0.25)))
+#' (NNidx == c(1,1,2,2))
+#' 
+#' # should be 0
+#' unique(DB[findNN(DB,DB)] - DB)
+findNN <- function (q, vec) {
+  .jinit()
+  .jaddClassPath("inst/java/deisotoper.jar")
+  .jclassPath()
+  
+  jFindNN <- .jnew("ch.fgcz.proteomics.R.UtilitiesR")
+  
+  idx <- .jcall(jFindNN, "[D", "findNNR", as.double(q), as.double(vec))
+  
+  idx
+}
