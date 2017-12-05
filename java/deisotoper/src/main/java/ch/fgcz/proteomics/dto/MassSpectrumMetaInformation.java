bee801872c39e3065be0ea1a5fa6190c99f26dec
Witold Wolski <wewolski@gmail.com>
2017-12-05 12:55:52 +0100

    Adding interfaces to facilitate refactoring from MassSpectrum to PeakList

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/dto/MassSpectrumMetaInformation.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/dto/MassSpectrumMetaInformation.java
@@ -1,4 +1,13 @@
 package ch.fgcz.proteomics.dto;
 
+import java.util.List;
+
 public interface MassSpectrumMetaInformation {
+
+    double getPeptideMass();
+
+    int getChargeState();
+
+    List<Double> getMz();
+    List<Double> getIntensity();
 }
