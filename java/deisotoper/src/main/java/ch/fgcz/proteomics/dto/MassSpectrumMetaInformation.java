8ab5c392b01850df305594cfaac50a52d2f7709b
Witold Wolski <wewolski@gmail.com>
2017-12-05 15:18:08 +0100

    removed getMZ and getIntensity from interface

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/dto/MassSpectrumMetaInformation.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/dto/MassSpectrumMetaInformation.java
@@ -3,11 +3,7 @@ package ch.fgcz.proteomics.dto;
 import java.util.List;
 
 public interface MassSpectrumMetaInformation {
-
     double getPeptideMass();
-
     int getChargeState();
 
-    List<Double> getMz();
-    List<Double> getIntensity();
 }
