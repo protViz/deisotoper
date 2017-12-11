592477a27252de1ca12b5ec7c11145a29f8c6d1b
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-12-11 15:14:54 +0100

    added tests

--- java/deisotoper/src/test/java/ch/fgcz/proteomics/fbdm/IsotopicSetTest.java
+++ java/deisotoper/src/test/java/ch/fgcz/proteomics/fbdm/IsotopicSetTest.java
@@ -7,11 +7,11 @@ package ch.fgcz.proteomics.fbdm;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import ch.fgcz.proteomics.dto.MassSpectrum;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class IsotopicSetTest {
@@ -56,11 +56,87 @@ public class IsotopicSetTest {
         PeakList peakList = new PeakList(massSpectrum);
 
         new IsotopicSet(peakList, peakList.getPeakList(), 1, config);
-
     }
 
     @Test
-    public void testRemoveDoubleClusterLeaveTripleCluster(){
-       assertEquals(0,1);
+    public void testRemoveDoubleClusterLeaveTripleCluster() {
+        Configuration config = new Configuration();
+        PeakList peakList = new PeakList();
+        peakList.add(new Peak(1, 100, 0));
+        peakList.add(new Peak(2, 200, 1));
+        peakList.add(new Peak(3, 300, 2));
+
+        List<IsotopicCluster> inputSet = new ArrayList<>();
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1), peakList.get(2)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(1), peakList.get(2)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+
+        List<IsotopicCluster> resultSet = IsotopicSet.removeDoubleClusterLeaveTripleCluster(inputSet);
+
+        assertEquals(1, resultSet.size());
+        assertEquals(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1), peakList.get(2)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()).toString(), resultSet.get(0).toString());
+    }
+
+    @Test
+    public void testRemoveDoubleClusterLeaveTripleClusterTwo() {
+        Configuration config = new Configuration();
+        PeakList peakList = new PeakList();
+        peakList.add(new Peak(1, 100, 0));
+        peakList.add(new Peak(2, 200, 1));
+        peakList.add(new Peak(4, 300, 2));
+        peakList.add(new Peak(5, 400, 3));
+
+        List<IsotopicCluster> inputSet = new ArrayList<>();
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(2), peakList.get(3)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+
+        List<IsotopicCluster> resultSet = IsotopicSet.removeDoubleClusterLeaveTripleCluster(inputSet);
+
+        assertEquals(2, resultSet.size());
+        assertEquals(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()).toString(), resultSet.get(0).toString());
+        assertEquals(new IsotopicCluster(Arrays.asList(peakList.get(2), peakList.get(3)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()).toString(), resultSet.get(1).toString());
+    }
+
+    @Test
+    public void testRemoveDoubleClusterLeaveTripleClusterThree() {
+        Configuration config = new Configuration();
+        PeakList peakList = new PeakList();
+        peakList.add(new Peak(1, 100, 0));
+        peakList.add(new Peak(2, 200, 1));
+        peakList.add(new Peak(3, 300, 2));
+        peakList.add(new Peak(5, 400, 3));
+        peakList.add(new Peak(6, 500, 4));
+        peakList.add(new Peak(7, 600, 5));
+
+        List<IsotopicCluster> inputSet = new ArrayList<>();
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1), peakList.get(2)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(1), peakList.get(2)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(3), peakList.get(4), peakList.get(5)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(3), peakList.get(4)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+        inputSet.add(new IsotopicCluster(Arrays.asList(peakList.get(4), peakList.get(5)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()));
+
+        List<IsotopicCluster> resultSet = IsotopicSet.removeDoubleClusterLeaveTripleCluster(inputSet);
+
+        assertEquals(2, resultSet.size());
+        assertEquals(new IsotopicCluster(Arrays.asList(peakList.get(0), peakList.get(1), peakList.get(2)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()).toString(), resultSet.get(0).toString());
+        assertEquals(new IsotopicCluster(Arrays.asList(peakList.get(3), peakList.get(4), peakList.get(5)), 1, peakList,
+                config.getIsotopicPeakDistance(), config.getDelta()).toString(), resultSet.get(1).toString());
     }
 }
