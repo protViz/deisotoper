61ffca871f91f8cbb0082dee93f20c37c9ea047e
Witold Wolski <wewolski@gmail.com>
2017-12-11 20:28:47 +0100

    removed <> for 1.6 compatibility and added Sonarcube

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/fbdm/IsotopicSet.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/fbdm/IsotopicSet.java
@@ -164,7 +164,7 @@ public class IsotopicSet {
     }
 
     // TODO : write a few tests.
-    protected List<IsotopicCluster> removeDoubleClusterLeaveTripleCluster(List<IsotopicCluster> isotopicClusters) {
+    protected static List<IsotopicCluster> removeDoubleClusterLeaveTripleCluster(List<IsotopicCluster> isotopicClusters) {
         List<IsotopicCluster> isotopicClusters2 = new ArrayList<IsotopicCluster>();
 
         for (IsotopicCluster cluster1 : isotopicClusters) {
