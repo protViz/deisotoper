55387cad29808dd1dab259714bb9c622d2ff8037
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-12-12 15:09:22 +0100

    refactored version

--- java/deisotoper/src/main/java/ch/fgcz/proteomics/Version.java
+++ java/deisotoper/src/main/java/ch/fgcz/proteomics/Version.java
@@ -6,49 +6,29 @@ package ch.fgcz.proteomics;
  */
 
 public class Version {
+    private static final String DTO = "Java Package: DTO, Date: 2017-12-04, Author: Lucas Schmidt";
+    private static final String FBDM = "Java Package: FBDM, Date: 2017-12-08, Author: Lucas Schmidt";
+    private static final String MSPY = "Java Package: MSPY (Deprecated), Date: 2017-12-04, Author: Lucas Schmidt";
+    private static final String MGF = "Java Package: MGF, Date: 2017-12-04, Author: Lucas Schmidt";
+    private static final String R = "Java Package: RAdapter, Date: 2017-12-04, Author: Lucas Schmidt";
+    private static final String UTILITIES = "Java Package: Utilities, Date: 2017-11-22, Author: Lucas Schmidt";
+    private static final String JAVA = "Java Version: " + System.getProperty("java.version");
+
     private Version() {
         // Empty constructor
-    }
-
-    private static String versionOfDto() {
-        return "Java Package: DTO, Date: 2017-12-04, Author: Lucas Schmidt";
-    }
-
-    private static String versionOfFbdm() {
-        return "Java Package: FBDM, Date: 2017-12-08, Author: Lucas Schmidt";
-    }
-
-    private static String versionOfMspy() {
-        return "Java Package: MSPY (Deprecated), Date: 2017-12-04, Author: Lucas Schmidt";
-    }
-
-    private static String versionOfMgf() {
-        return "Java Package: MGF, sDate: 2017-12-04, Author: Lucas Schmidt";
-    }
-
-    private static String versionOfRAdapter() {
-        return "Java Package: RAdapter, Date: 2017-12-04, Author: Lucas Schmidt";
-    }
-
-    private static String versionOfUtilities() {
-        return "Java Package: Utilities, Date: 2017-11-22, Author: Lucas Schmidt";
-    }
-
-    private static String javaVersion() {
-        return "Java Version: " + System.getProperty("java.version");
     }
 
     public static String version() {
         String linesep = System.getProperty("line.separator");
         StringBuilder sb = new StringBuilder();
 
-        sb.append(versionOfDto()).append(linesep);
-        sb.append(versionOfFbdm()).append(linesep);
-        sb.append(versionOfMspy()).append(linesep);
-        sb.append(versionOfMgf()).append(linesep);
-        sb.append(versionOfRAdapter()).append(linesep);
-        sb.append(versionOfUtilities()).append(linesep);
-        sb.append(javaVersion());
+        sb.append(DTO).append(linesep);
+        sb.append(FBDM).append(linesep);
+        sb.append(MSPY).append(linesep);
+        sb.append(MGF).append(linesep);
+        sb.append(R).append(linesep);
+        sb.append(UTILITIES).append(linesep);
+        sb.append(JAVA);
 
         return sb.toString();
     }
