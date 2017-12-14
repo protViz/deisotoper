61403ac4792d2c97ea5e0f726be1c350ae283252
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-12-14 09:33:03 +0100

    made travis work again

--- tests/testthat/test-findNN.R
+++ tests/testthat/test-findNN.R
@@ -9,11 +9,17 @@ test_that("test deisotoper::findNN", {
 
   # TEST 2
   DB <- sort(rnorm(100, mean=100, sd=10))
-  lapply(deisotoper::findNN(DB, DB) == 1:length(DB), expect_true)
+  test2 <- deisotoper::findNN(DB, DB) == 1:length(DB)
+  for (i in test2) {
+    expect_true(i)    
+  }
   
   # TEST 3 -- testing lower and upper index
   DB <- seq(-1, 1, length = 101)
   query <- c(-1000, 0, 0.001, 10, 10000)
   result <- c(1, 51, 51, 101, 101)
-  lapply((deisotoper::findNN(query, DB)) == result, expect_true)
+  test3 <- deisotoper::findNN(query, DB) == result
+  for (i in test3) {
+    expect_true(i)    
+  }
 })
