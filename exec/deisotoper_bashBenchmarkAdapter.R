21edb35e1f7b3dfeda14db4a1c378ce2c4b94438
LukeCaruzo <schmidt.lucas@googlemail.com>
2017-12-15 09:51:23 +0100

    deisotoper_bashBenchmarkAdapter

--- exec/deisotoper_bashBenchmarkAdapter.R
+++ exec/deisotoper_bashBenchmarkAdapter.R
@@ -4,9 +4,44 @@ args <- commandArgs(TRUE)
 
 library(deisotoper)
 
-jBenchmark(input = args[[1]], output = paste(args[[2]], "_fbdm_first.mgf", sep = ""), modus="first")
-if(file.exists("/srv/lucas1/deisotoper/properties/AminoAcidMassesWithCarbamidomethylDeamidationOxidation.properties")) {
-  jBenchmark(input = args[[1]], output = paste(args[[2]], "_fbdm_with_mod_first.mgf", sep = ""), modus="first", aamassfile = "/srv/lucas1/deisotoper/properties/AminoAcidMassesWithCarbamidomethylDeamidationOxidation.properties")
+.write_mgf_header <- function(filename){
+  FILE <- file(filename, "a")
+  
+  writeLines(as.character(paste("# deisotoper packageVersion ", packageVersion('deisotoper'))), FILE)
+  
+  writeLines(paste("# ",date(),sep=''), FILE)
+  
+  close(FILE)
 }
 
+.append_mgf <- function(obj, filename){
+  FILE <- file(filename, "a")
+  
+  writeLines("BEGIN IONS", FILE)
+  writeLines(paste("TITLE=",as.character(obj$title),sep=''), FILE)
+  writeLines(paste("PEPMASS=",as.character(obj$pepmass),sep=''), FILE)
+  writeLines(paste("CHARGE=",as.character(obj$charge),'+',sep=''), FILE)
+  writeLines(paste("SCANS=",as.character(obj$scans),sep=''), FILE)
+  writeLines(paste("RTINSECONDS=",as.character(obj$rtinseconds),sep=''), FILE)
+  writeLines(as.character(paste(obj$mZ, obj$intensity, sep=' ')), FILE)
+  writeLines("END IONS", FILE)                              
+  writeLines("", FILE)                              
+  close(FILE)
+}
+
+mgf <- function(obj, filename = paste(tempfile(), 'mgf', sep='.')){
+  message(paste("writing mgf to file", filename, "..."))
+  .write_mgf_header(filename = filename)
+  rv <- lapply(obj, function(x){.append_mgf(x, filename = filename)})
+  file.info(filename)
+}
+
+dtoper <- deisotoper() 
+
+psmset <- load(args[[1]])
+
+psmsetdeisotoped <- deisotope.list(dtoper, get(psmset))
+
+mgf(psmsetdeisotoped, paste(args[[2]], "_deisotoped.mgf", sep = ""))
+  
 rm(list=ls())
