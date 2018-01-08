17da637584ff51febde51badbfa1071642dad4fa
cpanse <cp@fgcz.ethz.ch>
2018-01-08 07:36:58 +0100

    version requirement for Java code at runtime

--- R/onAttach.R
+++ R/onAttach.R
@@ -7,6 +7,15 @@
 #'
 #' @importFrom utils packageVersion
 .onAttach <- function(lib, pkg){
+  
+  
+  .jinit()
+  jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+  if(substr(jv, 1L, 1L) == "1") {
+    jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+    if(jvn < 1.8) stop("Java 8 is needed for this package but not available")
+  } 
+  
 	if(interactive()){
 		version <- packageVersion('deisotoper')
 		packageStartupMessage("R Package: deisotoper, Version: ", version, "\n", .version())
