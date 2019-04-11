de7121d33373d9031021df1781e6f4e1b5a1d89a
wolski <wewolski@gmail.com>
2019-04-12 00:02:43 +0200

    attempt to fix by changing onLoad options

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-   options(java.parameters = "- Xmx10000000m") 
+   options(java.parameters = "-Xmx1000m") 
   rJava::.jpackage(pkgname, lib.loc = libname)
 }
